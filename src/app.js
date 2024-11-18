4a9bb20658c8addc66eca6a9abcc0d537573a81a
Niraj Parab <nirajparab007@gmail.com>
2024-11-18 16:05:32 +0530

    Unique routing based on string pattern and regex

--- src/app.js
+++ src/app.js
@@ -2,16 +2,47 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("this is dashboard");
+// app.use("/hello", (req, res) => {
+//   res.send("this is hello page");
+// });
+app.get("/ab?cd", (req, res) => {
+  // res.send("this is hello page");
+  res.send("zxcv called from get method");
+  
+});
+app.get("/zx*cv", (req, res) => {
+  // res.send("this is hello page");
+  res.send("zxcv called from get method");
+  
 });
 
-app.use("/hello", (req, res) => {
+
+app.post("/hello", (req, res) => {
+  res.send("hello called from post method");
+
+});
+app.delete("/hello", (req, res) => {
+  res.send("hello called from delete method");
+
+});
+app.patch("/hello", (req, res) => {
+  res.send("hello called from patch method");
+
+});
+
+app.put("/hello", (req, res) => {
+  res.send("hello called from put method");
+
+});
+app.use("/hello/123", (req, res) => {
   res.send("this is hello page");
 });
-
 app.use("/namskar", (req, res) => {
   res.send("this is namskar page");
+});
+
+app.use("/", (req, res) => {
+  res.send("this is dashboard");
 });
 
 app.listen(7777, () => {
