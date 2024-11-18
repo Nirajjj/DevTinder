fe64994fbbcbb2d1ab06374ed0a85ee305567caa
Niraj Parab <nirajparab007@gmail.com>
2024-11-18 18:08:11 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -11,10 +11,16 @@ app.get("/user/login", (req, res, next) => {
   res.send("login page");
 });
 app.get("/user/getuser",auth,  (req, res, next) => {
+  // throw new Error("getuser error");
   res.send("user data send");
 });
-app.use("/", (req, res) => {
-  res.send("this is dashboard");
+app.use("/", (err, req, res, next) => {
+  console.log(err.message);
+  
+  if(err){
+    res.status(500).send("something went wrong")
+  }
+  res.send("this is dashboard")
 });
 
 app.listen(7777, () => {
