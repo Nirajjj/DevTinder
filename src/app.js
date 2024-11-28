e297476b5b9432601c81e72892841e498b449bf6
Niraj Parab <nirajparab007@gmail.com>
2024-11-28 19:29:58 +0530

    email and password authentication

--- src/app.js
+++ src/app.js
@@ -1,16 +1,56 @@
 const express = require("express");
 const connectDb = require("./config/database");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
+
+// _id
+// 6741470014c9093accd84f17
+// firstName
+// "harryy"
+// lastName
+// "poter"
+// email
+// "harry@gmailcom"
+// password
+// "harry@123"
+// createdAt
+// 2024-11-23T03:07:44.034+00:00
+// updatedAt
+// 2024-11-23T04:33:54.980+00:00
+// __v
+// 0
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
+  const { firstName, lastName, email, password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = new User({ firstName, lastName, email, password: passwordHash });
   try {
     await user.save();
     res.send("user added successfully!");
   } catch (error) {
     res.status(400).send("data saving error!" + error.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("invalide credentioal");
+    }
+    const passwordValidate = await bcrypt.compare(password, user.password);
+    if (!passwordValidate) {
+      throw new Error("invalide credentioal");
+    } else {
+      res.send("login success");
+    }
+    // await user.save();
+    // res.send("user added successfully!");
+  } catch (error) {
+    res.status(400).send("error:" + error.message);
   }
 });
 
@@ -41,19 +81,6 @@ app.delete("/user", async (req, res) => {
 app.patch("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
   const object = req.body;
-
-  // firstName
-  // "harry"
-  // lastName
-  // "poter"
-  // email
-  // "harry@gmailcom"
-  // password
-  // "harry@123"
-  // createdAt
-  // 2024-11-23T03:07:44.034+00:00
-  // updatedAt
-  // 2024-11-23T03:07:44.034+00:00
   try {
     const approveUpdateKeys = [
       "firstName",
